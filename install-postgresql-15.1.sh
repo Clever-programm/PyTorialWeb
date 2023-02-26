e82f1adca52ccf03515258641098dda1fbc693b9
serveradmin <nonexist@tsixen.on>
2023-02-26 12:53:56 +0000

    finished postgres installation script

--- install-postgresql-15.1.sh
+++ install-postgresql-15.1.sh
@@ -1,35 +1,61 @@
 #!/bin/bash
 
-local_db_handler="./utils/db/postgres"
-logfile=$local_db_handler/postgresql/logfile.txt
+local_db_handler=$(pwd)/utils/db/postgres
+# logfile=$local_db_handler/postgresql/logfile.txt
+# templog=$(pwd)/logfile.txt
 
-touch $logfile
 
-echo logs at $logfile
+# echo $logfile, $templog
+# touch $logfile
+# touch $templog
+
+
+# echo logs at $logfile
+
 
 echo creating work directory
 mkdir -p $local_db_handler
 
+
 echo downloading postgresql
-curl --create-dir --output $local_db_handler/postgres.tar.gz https://ftp.postgresql.org/pub/source/v15.1/postgresql-15.1.tar.gz > $logfile
+#curl --create-dir --output $local_db_handler/postgres.tar.gz https://ftp.postgresql.org/pub/source/v15.1/postgresql-15.1.tar.gz # > $templog
+
 
 echo extracting postgresql
-tar -xvf $local_db_handler/postgres.tar.gz -C $local_db_handler > $logfile
+tar -xvf $local_db_handler/postgres.tar.gz -C $local_db_handler # > $templog
 
 
 echo creating symbolic link
-ln -s postgresql-15.1 $local_db_handler/postgresql > $logfile
+ln -s postgresql-15.1 $local_db_handler/postgresql # > $templog
 
 
 echo moving to the work directory
-cd $local_db_handler/postgresql > $logfile
+cd $local_db_handler/postgresql
 
 
 echo configure
-./configure > $logfile
+./configure --without-zlib # > $templog
 
 
-echo install
-make install -f GNUmakefile > $logfile
+echo make install
+make install -f GNUmakefile # > $templog
 
+echo post configure
+adduser postgres # > $templog
+
+mkdir -p /usr/local/pgsql/data
+chown postgres /usr/local/pgsql/data # > $templog
+
+su - postgres # > $templog
+
+/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data # > $templog
+
+/usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start # > $templog
+
+/usr/local/pgsql/bin/createdb test # > $templog
+
+/usr/local/pgsql/bin/psql test # > $templog
+
+
+# cat $templog > $logfile
 
